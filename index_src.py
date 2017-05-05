27b6b059175680db372b9f5781ff99e42a66cb7b
ThrowableFactory <ThrowableFactory@foxmail.com>
2017-05-05 12:48:51 +0800

    login reg and site ownership

--- index_src.py
+++ index_src.py
@@ -6,33 +6,25 @@ from flask_cors import CORS
 from bson.objectid import ObjectId
 from flask import request
 from pymongo import ReturnDocument
+from secret import get_secret
 import re
+from tld import get_tld
+import requests as r
 import simplejson as json
+import dns.resolver
+
+from flask_jwt_extended import JWTManager, jwt_required,\
+    create_access_token, get_jwt_identity
 
 app = Flask(__name__)
+app.secret_key = get_secret()
+jwt = JWTManager(app)
+
 CORS(app,automatic_options=False)
 client = MongoClient('mongodb://localhost:27017/')
 db = client.src_index
-collection = db.NewSites
-
-@app.route('/sites/<int:page>')
-def get(page):
-    pagesize = 50
-    cursor = collection.find().sort('createdAt',pymongo.ASCENDING).skip(page*50).limit(50)
-    count = int(cursor.count() / pagesize)
-    res = {"pages":count,"res":cursor}
-    return dumps(res)
-
-
-@app.route('/sites/group/<string:group>/page/<int:page>')
-def getByGroup(group,page):
-    pagesize = 50
-    if(group == '全部'):
-        return get(page)
-    cursor = collection.find({"groups":group}).sort('createdAt',pymongo.ASCENDING).skip(page*50).limit(50)
-    count = int(cursor.count() / pagesize)
-    res = {"pages":count,"res":cursor}
-    return dumps(res)
+site_collection = db.NewSites
+user_collection = db.Users
 
 @app.route('/sites/search', methods = ['POST'])
 def hybridSearch():
@@ -96,7 +88,7 @@ def hybridSearch():
     if len(findObject['$and'])<1:
         del findObject['$and']
 
-    cursor = collection.find(findObject).skip(skip).limit(page_size)
+    cursor = site_collection.find(findObject).skip(skip).limit(page_size)
     if query_sort is not None and len(query_sort)>0:
         query_sort_tupple = []
         for e in query_sort:
@@ -112,23 +104,111 @@ def hybridSearch():
     res = {"pages":count,"res":cursor}
     return dumps(res)
 
-@app.route('/sites')
-def getAll():
-    return dumps(collection.find().sort('createdAt',pymongo.ASCENDING))
-
 @app.route('/site/<string:hash>', methods = ['DELETE'])
 def delete(hash):
-    collection.delete_one({"_id":ObjectId(hash)})
+    site_collection.delete_one({"_id":ObjectId(hash)})
     return 'true'
 
-@app.route('/site/<string:id>', methods = ['POST','OPTIONS'])
+@app.route('/site/<string:id>', methods = ['POST'])
 def update(id):
     updateInfo = request.get_json()
     try:
-        res = dumps(collection.find_one_and_update({'_id':ObjectId(id)},updateInfo,return_document=ReturnDocument.AFTER))
+        res = dumps(site_collection.find_one_and_update({'_id':ObjectId(id)},updateInfo,return_document=ReturnDocument.AFTER))
     except Exception as e:
         res = str(e)
     return res
+
+
+# Provide a method to create access tokens. The create_access_token()
+# function is used to actually generate the token
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    expected = user_collection.find_one({"username":username})
+
+    if expected is None or password != expected.password:
+        return json.dumps({"msg": "Bad username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    ret = {'access_token': create_access_token(identity=username)}
+    return json.dumps(ret), 200
+
+# Provide a method to create access tokens. The create_access_token()
+# function is used to actually generate the token
+@app.route('/owned/<string:id>/file', methods=['GET'])
+@jwt_required
+
+def checkOwnedFile(id):
+    current_user = get_jwt_identity()
+    site = site_collection.find({"_id":ObjectId(id)})
+    owned = False
+    if site is not None:
+        try:
+            res = r.get('http://'+get_tld(site.url)+'/'+current_user._id)
+            if current_user._id in res.text:
+                owned = True
+        except:
+            try:
+                res = r.get('http://www.'+get_tld(site.url)+'/'+current_user._id)
+                if current_user._id in res.text:
+                    owned = True
+                else:
+                    owned = False
+            except:
+                owned = False
+        if(owned):
+            res = dumps(site_collection.find_one_and_update({'_id': ObjectId(id)}, {"$set":{owned:current_user._id}},
+                                                            return_document=ReturnDocument.AFTER))
+            return res
+        else:
+            return json.dumps(False)
+
+@app.route('/owned/<string:id>/cname', methods=['GET'])
+@jwt_required
+def checkOwned(id):
+    current_user = get_jwt_identity()
+    site = site_collection.find({"_id":ObjectId(id)})
+    url = 'http://'+current_user._id+get_tld(site.url)
+    cname = dns.resolver.query(url, 'CNAME')
+    owned = False
+    for i in cname.response.answer:
+        for j in i.items:
+            if 'leyline.cc' in j.to_text():
+                owned = True
+                break
+    if (owned):
+        res = dumps(site_collection.find_one_and_update({'_id': ObjectId(id)}, {"$set": {owned: current_user._id}},
+                                                        return_document=ReturnDocument.AFTER))
+        return res
+    else:
+        return json.dumps(False)
+
+
+
+# Provide a method to create access tokens. The create_access_token()
+# function is used to actually generate the token
+@app.route('/reg', methods=['POST'])
+def reg():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if checkNameExists(username):
+        return False
+    else:
+        user_collection.insert({"username":username,"password":password})
+
+@app.route('/reg/exists/<string:username>', methods=['GET'])
+def checkNameExists(username):
+    expected = user_collection.find_one({"username": username})
+    return expected is not None
+
+
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return json.dumps({'hello_from': current_user}), 200
 
 
 if __name__ == '__main__':
